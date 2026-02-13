88fb2035e052235cd2edca20f810ed2557acf486
Raunak Bagaria <146432593+raunak-bagaria@users.noreply.github.com>
2026-02-13 11:42:14 +0530

    Add column category with index

--- backend/create.sql
+++ backend/create.sql
@@ -10,11 +10,13 @@ create table if not exists public.events (
   address text,
   lat double precision not null,
   long double precision not null,
-  organizer text
+  organizer text,
+  category VARCHAR
 );
 
 create index if not exists events_startdate_idx on public.events ("startDate");
 create index if not exists events_lat_long_idx on public.events (lat, long);
+create index events_category on public.events (category)
 
 -- Use url as the unique identity key for upserts.
 create unique index if not exists events_url_uidx
